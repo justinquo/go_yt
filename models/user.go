5ff145a1faf1c908621c3e1d87fbd9fbbdee172e
justinquo <justinyquo@gmail.com>
2023-08-20 22:50:25 -0400

    added controllers and routes

--- models/user.go
+++ models/user.go
@@ -1,13 +1,13 @@
 package models
 
 type Address struct {
-	State   string
-	City    string
-	Pincode int
+	State   string `json: "state" bason: "state"`
+	City    string `json: "city" bason: "city"`
+	Pincode int    `json: "pincode" bason: "pincode"`
 }
 
 type User struct {
-	Name    string
-	Age     int
-	Address Address
+	Name    string  `json: "name" bason: "user_name"`
+	Age     int     `json: "age" bason: "user_age"`
+	Address Address `json: "address" bason: "user_address"`
 }
